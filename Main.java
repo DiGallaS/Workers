97a651b160492de6e6ddec40037b3b1487d151c8
Дмитрий Галашев <galashev1990@gmail.com>
2022-09-25 12:31:06 +0300

    the first solution

---
+++ Main.java
@@ -0,0 +1,10 @@
+package Work;
+
+public class Main {
+    public static void main(String[] args) {
+        Worker.OnTaskDoneListener listener = System.out::println;
+        Worker.OnTaskErrorListener listener2 = System.out::println;
+        Worker worker = new Worker(listener, listener2);
+        worker.start();
+    }
+}
